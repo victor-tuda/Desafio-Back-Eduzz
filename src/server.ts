3c071696f1f757d44b0817af33f5c6688d4593f5
victor-tuda <victor_gtuda@outlook.com>
2024-06-15 10:21:25 -0300

    refactor: app initialize after db connection

--- src/server.ts
+++ src/server.ts
@@ -2,21 +2,24 @@ import 'reflect-metadata';
 import express from 'express';
 import { AppDataSource } from './data-source';
 import routers from './routes/routes';
-import cors from 'cors';
+import handleErrors from './middlewares/error';
 
-// Criando uma aplicação express
-const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(routers);
 
 // Inicializando a conexão com o banco de dados
 // e iniciando o server apenas se a conexão com o banco for bem sucedida
 AppDataSource.initialize()
   .then(async () => {
+    // Criando uma aplicação express
+    const app = express();
+
+    app.use(express.json());
+    app.use(routers);
+    app.use(handleErrors);
+
     app.listen(3000, () => {
       console.log('Server started');
     });
   })
   .catch(error => console.log(error));
+
