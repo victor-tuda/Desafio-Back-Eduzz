9b22b363f8af557ce3f5e3fae5d44674c529dc73
victor-tuda <victor_gtuda@outlook.com>
2024-06-15 13:38:10 -0300

    feat: routes using jwt

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,39 @@
+import { accountRepository } from './../repositories/AccountRepository';
+import {Request, Response, NextFunction} from 'express'
+import Exception from '../helpers/api-errors' 
+import jwt from 'jsonwebtoken'
+
+type JwtPayload = {
+    id: string
+}
+
+export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    try{
+        const { authorization } = req.headers
+            
+        if (!authorization) {
+            throw new Exception(401, 'E-mail already exists', req.path); // TODO: refactor this line
+        }
+
+        const token = authorization.split(' ')[1];
+
+        const { id } = jwt.verify(token, process.env.JWT_PASS ?? '') as JwtPayload
+
+        const accountId = await accountRepository.findOneBy({ id });
+
+        // Throw an error if the id does not exists
+        if (!accountId) {
+            throw new Exception(401, 'E-mail already exists', req.path); // TODO: refactor this line
+        }
+
+        const { password: _, ...loggedAccount } = accountId
+
+        req.account = loggedAccount
+
+        next()
+
+    } catch(error){
+        next(new Exception(500, "Internal Server Error", req.path)); // TODO: refactor this line
+    }
+
+}
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,16 @@
+import { authMiddleware } from './../middlewares/authMiddleware';
 import Router from 'express';
 import AccountController from '../controllers/AccountController';
+import LoginController from '../controllers/LoginController';
 
 const routers = Router();
 
-routers.get('/');
-routers.post('/accounts', AccountController.create);
+routers.post('/account', AccountController.create); // Create a new account
+routers.post('/login', LoginController.login); // Login to receive the token
+
+routers.use(authMiddleware) // Below of this line, all the routes will need the authentication token
+
+routers.get('/account/balance', AccountController.getAccount); // Get informations about the account
 
 export default routers; 
 
