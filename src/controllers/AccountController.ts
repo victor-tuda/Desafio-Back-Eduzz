fa2703baa10082b6e7d96e192956993a4ba816d3
victor-tuda <victor_gtuda@outlook.com>
2024-06-15 10:22:23 -0300

    feat: create account

--- src/controllers/AccountController.ts
+++ src/controllers/AccountController.ts
@@ -1,19 +1,35 @@
-import { Request, Response, Router } from 'express';
-import { Account } from '../entities/Account';
-import AccountService from '../services/AccountService'
-import { IAccount } from '../interfaces/account'
+import { Request, Response, NextFunction } from 'express';
+import { accountRepository } from '../repositories/AccountRepository';
+import Exception from '../helpers/api-errors';
+import bcrypt from 'bcrypt';
 
-const accountRouter = Router();
+class AccountController {
+    async create(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { email, password } = req.body;
 
-accountRouter.get('/', async (_req: Request, res: Response): Promise<Response> => {
-    const accounts = await AccountService.getAccount();
-    console.log("teste")    
-    return res.status(200).json(accounts);
-})
+            const accountExists = await accountRepository.findOneBy({ email });
+    
+            if (accountExists) {
+                throw new Exception(401, 'E-mail already exists', req.path);
+            }
+    
+            const hashPassword = await bcrypt.hash(password, 10);
+    
+            const newAccount = accountRepository.create({
+                email,
+                password: hashPassword
+            });
+    
+            await accountRepository.save(newAccount);
+    
+            const { password: _, ...accountData } = newAccount;
+    
+            return res.status(201).json(accountData);
+        } catch (error) {
+            next(new Exception(500, "Internal Server Error", req.path));
+        }
+    }
+}
 
-
-export default accountRouter
-
-
-
-
+export default new AccountController;
