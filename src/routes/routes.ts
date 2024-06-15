5e0ae369ce4cf41e68fcd504c23911a694a95c26
victor-tuda <victor_gtuda@outlook.com>
2024-06-15 10:15:58 -0300

    feat: route to create an account

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,10 @@
 import Router from 'express';
-import accountRouter from '../controllers/AccountController';
+import AccountController from '../controllers/AccountController';
 
 const routers = Router();
 
-routers.use('/', accountRouter);
-routers.use('/accounts', accountRouter);
+routers.get('/');
+routers.post('/accounts', AccountController.create);
 
 export default routers; 
 
