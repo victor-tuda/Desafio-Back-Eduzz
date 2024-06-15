367b70f891e85dcd1cf8db4702f7bef31b938499
victor-tuda <victor_gtuda@outlook.com>
2024-06-15 10:20:51 -0300

    feat: enviorement variables

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,18 +1,21 @@
 import 'reflect-metadata';
+import 'dotenv/config'
 import { DataSource } from 'typeorm';
 import { Account } from './entities/Account';
 import { CreateAccount1718324356043 } from './database/migrations/1718324356043-CreateAccount';
 
+const port = process.env.DB_PORT as number | undefined
+
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: 'eduzz-mysql.cinb14ueuryw.us-west-2.rds.amazonaws.com',
-  port: 3306,
-  username: 'admin',
-  password: 'Eduzz_001',
-  database: 'desafio_crypto',
+  host: process.env.DB_HOST,
+  port: port,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
   synchronize: true,
   logging: false,
-  entities: [Account],
-  migrations: [CreateAccount1718324356043],
+  entities: [`${__dirname}/**/entities/*.{ts,js}`],
+  migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
   subscribers: []
 });
