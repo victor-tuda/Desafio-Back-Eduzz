1580edd738c06ce336d81da216cabacc963230a6
victor-tuda <victor_gtuda@outlook.com>
2024-06-15 22:29:28 -0300

    feat: get crypto cotation

--- src/interfaces/crypto.ts
+++ src/interfaces/crypto.ts
@@ -1,11 +1,19 @@
 export interface TickerResponse {
-    ticker: {
-      high: string;
-      low: string;
-      vol: string;
-      last: string;
-      buy: string;
-      sell: string;
-      date: number;
-    };
-  }
+  ticker: {
+    high: string;
+    low: string;
+    vol: string;
+    last: string;
+    buy: string;
+    sell: string;
+    date: number;
+  };
+}
+
+export interface CotationResponse {
+  btc: {
+    cotation: string;
+    buy: string;
+    sell: string;
+  };
+}
--- src/services/CryptoService.ts
+++ src/services/CryptoService.ts
@@ -1,16 +1,27 @@
-import { TickerResponse } from '../interfaces/crypto';
+import { CotationResponse, TickerResponse } from '../interfaces/crypto';
 import CryptoApi from '../support/Libraries/CryptoApi';
 import Exception from '../helpers/api-errors';
 import { Request } from 'express';
 
 class CryptoService {
-  public async getCryptoApi(req: Request): Promise<TickerResponse> {
+  public async getCryptoApi(req: Request): Promise<CotationResponse> {
     try {
       const crypto = await CryptoApi.getCryptoApi(req);
-      return crypto;
+      const cryptoCotation = this.cotationData(crypto);
+      return cryptoCotation;
     } catch (error) {
       throw new Exception(500, 'Internal Server Error', req.path);
     }
+  }
+
+  private cotationData(crypto: TickerResponse): CotationResponse {
+    return {
+      btc: {
+        cotation: crypto.ticker.last,
+        buy: crypto.ticker.buy,
+        sell: crypto.ticker.sell
+      }
+    };
   }
 }
 
